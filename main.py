76ac143dcffaf07240373a174e6a6edb9b5e1e4b
Alexander Melnikov <Msan90546@mail.ru>
2022-11-03 19:57:54 +0300

    Add database and design

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from PyQt5 import uic
 class InfoCoffee(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('main.ui')
+        uic.loadUi('main.ui', self)
         self.show_info()
 
     def show_info(self):
